c4ee0b6defa4847d1910cb891b1fd012eafa70f3
deepibs45 <109540675+deepibs45@users.noreply.github.com>
2022-07-22 16:28:56 +0530

    Update JenkinsDemo.java

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -1,25 +1,8 @@
-
-import java.util.Scanner;
-
-public class JenkinsDemo
+public class Hello
 {
-    public static void main(String args[])
-    {
-        int a,b,c,sum;
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter the first number");
-        a=sc.nextInt();
-        System.out.println("enter second number");
-        b=sc.nextInt();
-        System.out.println("enter third number");
-        c=sc.nextInt();
-        sum=sum(a,b,c);
-        System.out.println("the sum of three numbers is"+sum);
-    }
-
-    private static int sum(int a, int b, int c)
-    {
-        int sum=a+b+c;
-        return sum;
-    }
+   public static void main(String args[])
+   {
+       System.out.println("Hello Java");
+   }
+           
 }
